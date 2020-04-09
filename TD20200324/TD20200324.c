7de0608814012ca0eae1357cb19e9524769d87b7
Pierre BRESSY <pierre.bressy@heig-vd.ch>
2020-04-09 17:10:14 +0200

    Update TD20200324.c

--- TD20200324/TD20200324.c
+++ TD20200324/TD20200324.c
@@ -40,17 +40,15 @@ void ws(char *s)
     bool inWord = false;
     uint32_t wordIndex = 0;
     uint32_t index = 0;
-    uint32_t numWords = 0;
     uint32_t k = 0;
     char **word = NULL;
+    char **ptr = NULL; // result of realloc for word table
     char *start = NULL;
     char *stop = NULL;
-    char *p = NULL;
     uint32_t length = 0;
     bool wordAlreadyExist = false;
 
     // 1. get number of word in s
-    numWords = wc(s);
 
     // 2. allocate the word table
     // each cell of word will contain a word
@@ -63,7 +61,7 @@ void ws(char *s)
     // word[numWords-1] <- "suscipit"
 
     puts("*** Words in the dictionnary ***");
-    word = (char **)malloc(numWords * sizeof(char *));
+    word = NULL;
 
     while (s[index])
     {
@@ -113,32 +111,60 @@ void ws(char *s)
             // if not, add word to the table
             if (wordAlreadyExist == false)
             {
-                // allocate memory for the word in the table
-                word[wordIndex] = (char *)calloc(length + 1, sizeof(char)); // +1 for the '\0' to the end
-                // copy from string to word table (no need to add a \0', the calloc set the table to zero :)
-                memcpy(word[wordIndex], start, length);
+                ptr = (char **)realloc(word, (1 + wordIndex) * sizeof(char *)); // adjust the size of word
+                if (ptr == NULL)
+                {
+                    puts("Error during realloc.");
+                }
+                else
+                {
+                    // use the new allocated area
+                    word = ptr;
 
-                // check the content of the table
-                printf("%s\n", word[wordIndex]);
+                    // allocate memory for the word in the table
+                    word[wordIndex] = (char *)calloc(length + 1, sizeof(char)); // +1 for the '\0' to the end
+                    // copy from string to word table (no need to add a \0', the calloc set the table to zero :)
+                    memcpy(word[wordIndex], start, length);
 
-                wordIndex++;
+                    // check the content of the table
+                    printf("%s\n", word[wordIndex]);
+
+                    wordIndex++;
+                }
             }
         }
         index++;
     }
     printf("%u different word(s).\n", wordIndex);
+
+    // free each word
+    for (k = 0; k < wordIndex; k++)
+    {
+        if (word[k] != NULL)
+        {
+            free(word[k]);
+            word[k] = NULL;
+        }
+    }
+
+    // free of word table
+    if (word != NULL)
+    {
+        free(word);
+        word = NULL;
+    }
+
     return;
 }
 
 int main(int argc, char *argv[])
 {
     eErrorCode returnCode = E_NO_ERROR;
-    const char *filename = "text2.txt"; //argv[1];
+    const char *filename = argv[1]; // may check argc...
     FILE *f = NULL;
     uint32_t fileSize = 0;
     char *text = NULL;
     uint32_t wordCount = 0;
-    uint32_t k = 0;
 
     // open the file as a text file
     f = fopen(filename, "r");
